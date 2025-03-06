1fbfc33535f5883cb4d8c47a81a59dfa490b4b37
Le Minh Khanh <khanhle070900@gmail.com>
2025-03-06 10:34:26 +0700

    -

---
+++ e-commerce/e-commerce/Areas/Area/Controllers/HomeAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace e_commerce.Areas.Area.Controllers
+{
+    public class HomeAdminController : Controller
+    {
+        // GET: Area/HomeAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
